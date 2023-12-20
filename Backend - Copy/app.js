4fedfe1181d8a43dc3d10c5a7f1a814c33f95577
Taskeen Haider <151015426+taskeen598@users.noreply.github.com>
2023-12-20 17:01:59 +0500

    Add files via upload

---
+++ Backend - Copy/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors');
+const app = express();
+require('./db/connectdb')
+const usersRouter = require('./Routers/UserRouter')
+
+// * Connecting To Port
+const port = 1126;
+
+// * Automatically parse incoming JSON to an object so we access it in our request handlers
+app.use(express.json())
+
+app.use(express.urlencoded({extended: false}))
+    
+app.use(usersRouter);
+
+// * listening To Port
+app.listen(port, () => {
+    console.log(`This is the ${port} active port! Wait for DB Connection...`);
+});
